ab5ad8e140db2bec2786d5db48f750f698460fe6
haospring <66471298+haospring@users.noreply.github.com>
2021-01-25 15:04:01 +0800

    User

--- src/main/java/com/neu/his/pojo/User.java
+++ src/main/java/com/neu/his/pojo/User.java
@@ -1,5 +1,105 @@
 package com.neu.his.pojo;
 
 public class User {
-	
+	private int id;
+	private String userName;
+	private String password;
+	private String realName;
+	private String useType;
+	private String docTitleID;
+	private String isScheduling;
+	private int deptID;
+	private int registLeID;
+	private int delMark;
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+	public String getUseType() {
+		return useType;
+	}
+
+	public void setUseType(String useType) {
+		this.useType = useType;
+	}
+
+	public String getDocTitleID() {
+		return docTitleID;
+	}
+
+	public void setDocTitleID(String docTitleID) {
+		this.docTitleID = docTitleID;
+	}
+
+	public String getIsScheduling() {
+		return isScheduling;
+	}
+
+	public void setIsScheduling(String isScheduling) {
+		this.isScheduling = isScheduling;
+	}
+
+	public int getDeptID() {
+		return deptID;
+	}
+
+	public void setDeptID(int deptID) {
+		this.deptID = deptID;
+	}
+
+	public int getRegistLeID() {
+		return registLeID;
+	}
+
+	public void setRegistLeID(int registLeID) {
+		this.registLeID = registLeID;
+	}
+
+	public int getDelMark() {
+		return delMark;
+	}
+
+	public void setDelMark(int delMark) {
+		this.delMark = delMark;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", realName=" + realName
+				+ ", useType=" + useType + ", docTitleID=" + docTitleID + ", isScheduling=" + isScheduling + ", deptID="
+				+ deptID + ", registLeID=" + registLeID + ", delMark=" + delMark + "]";
+	}
 }
